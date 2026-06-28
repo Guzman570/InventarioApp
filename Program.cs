0b9ff31a0581c7d6e6747450212376edd50af711
Gabriel Guzman <guzmangabriel570@gmail.com>
2026-06-28 10:57:15 -0600

    Nuevo archivo README.ms

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+//============================
+//SISTEMA DE INVENTARIO - CLASE 1
+//ESTADO: mensaje de bienvenida
+//============================
+using System.Reflection;
+
+var assembly = Assembly.GetExecutingAssembly();
+var version = assembly.GetName().Version;
+
+Console.WriteLine("========================================");
+Console.WriteLine("     SISTEMA DE GESTION DE INVENTARIO   ");
+Console.WriteLine("========================================");
+Console.WriteLine();
+Console.WriteLine($"Version: {version}");
+Console.WriteLine($"Plataforma: {Environment.OSVersion}");
+Console.WriteLine($".NET version: {Environment.Version}");
+Console.WriteLine();
+Console.WriteLine("Estructurad del proyecto:");
+Console.WriteLine("configuracion .csproj");
+Console.WriteLine("carpeta src/ creada");
+Console.WriteLine("Metadatos configurados");
+Console.WriteLine();
+Console.WriteLine("proximo paso: agregar argumentos CLI y configuracion de repositorio en github");
---
+++ README.md
@@ -0,0 +1,21 @@
+# sistema de gestion de inventarios
+proyecto del curso **fundamentos de .NET** - platzi
+
+## Requisitos
+- .NEt 10 SDK
+
+## Como ejecutar
+dotnet run
+
+## Estructura del proyecto
+
+InventarioApp
+-Program.cs # Punto de entrada
+-InventarioApp.csproj # Configuracion
+-.gitignore # Archivos ignorados por git
+
+## Autor
+Gabriel Guzman
+
+## Progreso del curso
+- [X] modulo 1: El ecosistema .NET
